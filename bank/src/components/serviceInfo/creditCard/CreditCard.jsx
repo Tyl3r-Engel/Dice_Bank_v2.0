a6e5e565722d41961d50e8d28bae74e002d7f3a7
Tyler Engel <engelcrag@gmail.com>
2022-07-06 10:03:51 -0700

    added content to credit card page

--- bank/src/components/serviceInfo/creditCard/CreditCard.jsx
+++ bank/src/components/serviceInfo/creditCard/CreditCard.jsx
@@ -1,20 +1,30 @@
-import { Grid } from '@mui/material'
-import React from 'react'
-import Footer from '../../footer/Footer'
-import NavBar from '../../navBar/NavBar'
+import React from 'react';
+import { Grid } from '@mui/material';
+import NavBar from '../../navBar/NavBar';
+import Footer from '../../footer/Footer';
+import { CreditCardInfo } from './CreditCardInfo';
+import ServiceElement from '../ServiceElement';
+import { Box } from '@mui/system';
+import backgroundImage from '../serviceBackground.png';
 
 export default function CreditCard() {
-  return(
+  return (
     <Grid container direction='column'>
-      <Grid item xs={12}>
-        <NavBar />
-      </Grid>
-      <Grid item xs={12}>
-        <p>this is Credit Card</p>
-      </Grid>
-      <Grid item xs={12}>
-        <Footer />
-      </Grid>
+    <Grid item xs={12} >
+      <NavBar />
     </Grid>
+
+    <Grid item xs={12}>
+      <Box sx={{ backgroundImage : `url(${backgroundImage})`, padding : '1em'}}>
+        {
+          CreditCardInfo.map((element, index) => <ServiceElement key={`${index} ccem`} element={element}/> )
+        }
+      </Box>
+    </Grid>
+
+    <Grid item xs={12}>
+      <Footer />
+    </Grid>
+  </Grid>
   )
 }
